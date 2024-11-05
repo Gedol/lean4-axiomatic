c28a58cbb3f0a91f6fe36be6f450c037c80acb12
Charles Ruhland <cruhland@alumni.cmu.edu>
2024-11-05 07:10:51 -0800

    resolve errors by making ident an outParam

--- Lean4Axiomatic/ClassicalAlgebra/Group2.lean
+++ Lean4Axiomatic/ClassicalAlgebra/Group2.lean
@@ -29,43 +29,29 @@ For testing purposes just a subset of what we'd really have.
  -/
 class Props
     (α : Type) [EqvOp α]
-    (binop : semiOutParam (α → α → α)) (ident : semiOutParam α) [Ops α]
+    (binop : semiOutParam (α → α → α)) (ident : outParam α) [Ops α]
     :=
-  _substL {x y z : α} : x ≃ y → binop x z ≃ binop y z
-  _substR {x y z : α} : x ≃ y → binop z x ≃ binop z y
+  substL {x y z : α} : x ≃ y → binop x z ≃ binop y z
+  substR {x y z : α} : x ≃ y → binop z x ≃ binop z y
   assoc {x y z : α} : binop (binop x y) z ≃ binop x (binop y z)
   identL {x : α} : binop ident x ≃ x
   identR {x : α} : binop x ident ≃ x
   inverseL (x : α) : binop (inverse x) x ≃ ident
   inverseR (x : α) : binop x (inverse x) ≃ ident
 
-def substL
-    {α : Type} [EqvOp α] {binop : α → α → α} {ident : α}
-    [Ops α] [Props α binop ident] {x y z : α}
-    : x ≃ y → binop x z ≃ binop y z
-    :=
-  Props._substL ident
-
-def substR
-    {α : Type} [EqvOp α] {binop : α → α → α} {ident : α}
-    [Ops α] [Props α binop ident] {x y z : α}
-    : x ≃ y → binop z x ≃ binop z y
-    :=
-  Props._substR ident
-
 export Props (
-  inverseL inverseR assoc identL identR
+  inverseL inverseR assoc identL identR substL substR
 )
 
 /-- All axioms for generic types to form a Group. -/
 class Group2
     (α : Type) [eqv : EqvOp α]
-    (binop : semiOutParam (α → α → α)) (ident : semiOutParam α)
+    (binop : semiOutParam (α → α → α)) (ident : outParam α)
     :=
   toOps : Group2.Ops α
   toProps : Group2.Props α binop ident
 
-attribute [instance] Group2.toOps  -- ← Error here "cannot find synthesization
+attribute [instance] Group2.toOps
 attribute [instance] Group2.toProps
 
 local instance group_mul_op_inst
@@ -79,10 +65,8 @@ local instance group_subst_inst
     {α : Type} [EqvOp α] {binop : α → α → α} {ident : α} [Group2 α binop ident]
     : AA.Substitutive₂ (α := α) (· * ·) AA.tc (· ≃ ·) (· ≃ ·)
     := {
-  substitutiveL := { subst₂ := λ (_ : True) => substL (ident := ident) }
-   -- cruhland: still getting an error here; i think bc `ident` is not used,
-   -- so Lean is unable to determine a value for it?
-  substitutiveR := { subst₂ := λ (_ : True) => substR (ident := ident) }
+  substitutiveL := { subst₂ := λ (_ : True) => substL }
+  substitutiveR := { subst₂ := λ (_ : True) => substR }
 }
 
 variable {α : Type} [EqvOp α] {binop : α → α → α} {ident : α}
@@ -101,8 +85,8 @@ theorem group_cancelL
     _ ≃ y               := Rel.refl
     _ ≃ ident * y       := Rel.symm identL
     _ ≃ ((x⁻¹) * x) * y := substL (Rel.symm (inverseL x))
-    _ ≃ (x⁻¹) * (x * y) := assoc ident
+    _ ≃ (x⁻¹) * (x * y) := assoc
     _ ≃ (x⁻¹) * (x * z) := substR ‹x * y ≃ x * z›
-    _ ≃ (x⁻¹ * x) * z   := Rel.symm (assoc ident)
+    _ ≃ (x⁻¹ * x) * z   := Rel.symm assoc
     _ ≃ ident * z       := substL (inverseL x)
     _ ≃ z               := identL
